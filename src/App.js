4179cd04e48e0107aec199ced869caa23a834d43
tirumalarowthu <tirumalarowthuv@gmail.com>
2024-04-29 12:33:28 +0530

    Hello world check

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import OneMore from './OneMore';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
-      <Routes>
-        <Route element ={<Homepage/>} path = "/" />
-        <Route path = "/check" element ={<Check/>}></Route>
-        <Route path='/onemore' element={<OneMore/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h2>Hello world </h2>
+      <h4>How are you ? </h4>
+    </div>
 
     
    
@@ -20,3 +17,27 @@ function App() {
 }
 
 export default App;
+
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import './App.css';
+// import Check from './Check';
+// import Homepage from './Homepage';
+// import OneMore from './OneMore';
+
+// function App() {
+//   return (
+//     <BrowserRouter basename='/'>
+//       <Routes>
+//         <Route element ={<Homepage/>} path = "/" />
+//         <Route path = "/check" element ={<Check/>}></Route>
+//         <Route path='/onemore' element={<OneMore/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+
+    
+   
+//   );
+// }
+
+// export default App;
+
