ef68a58d7987d0d738e150f5afb189944c73bfa3
tirumalarowthu <tirumalarowthuv@gmail.com>
2024-04-29 14:29:57 +0530

    Hello world with data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import Check from './Check';
 import Homepage from './Homepage';
 import OneMore from './OneMore';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    const response = axios.get("https://jsonplaceholder.typicode.com/users").
+    then(res => {
+      setData(res.data)
+
+    }).catch(err => console.log(err))
+
+  })
   return (
     <div>
       <h2>Hello world </h2>
       <h4>How are you ? </h4>
+      {
+        data && data.length>0 && data.map(item =>{
+          return <div>{item.name} 
+          <h3>{item.username}</h3>
+          <h3>{item.email}</h3>
+          </div>
+        })
+      }
       <form onSubmit={()=>alert("form sumbitted .")}>
         <div> 
           <input name="veera" type='email'/>
